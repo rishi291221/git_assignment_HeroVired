36f5e1738cf144473396952ce7a17782a0038ee4
rishi291221 <darshini.nka@gmail.com>
2026-05-10 18:45:45 +0530

    Initial calculator application added

---
+++ Untitled-1.py
@@ -0,0 +1,7 @@
+n=0
+a = [1,2,3]
+for i in a:
+    print a[i]
+
+
+    
--- maths.py
+++ maths.py
@@ -1,4 +1,5 @@
 import math
+from turtle import width
 
 class Calculator:
 
@@ -34,3 +35,30 @@ if __name__ == "__main__":
 
     num3 = 25
     print(f"The square root of {num3} = {calculator.square_root(num3)}")
+    #import math
+
+class GeometryCalculator:
+
+    def calculate_circle_area(self, radius):
+
+        return math.pi * radius ** 2
+
+def calculate_rectangle_area(self, length, width):
+
+    return length * width
+
+if __name__ == "__main__":
+
+    calculator = GeometryCalculator()
+
+# TODO: Implement the feature to calculate the area of a circle
+
+radius = 5
+
+print ("The area of the circle with radius {radius} = {calculator.calculate_circle_area(radius)}")
+# TODO: Implement the feature to calculate the area of a rectangle 
+length = 10
+
+width = 6
+
+print(f"The area of the rectangle with length {length} and width {width} = {calculator.calculate_rectangle_area(length, width)}")
